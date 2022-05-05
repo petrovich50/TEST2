ef1436336968f59501bbd8766fe7ab3a256dc39f
DBandurin <dbandurin@gmail.com>
2022-05-05 21:36:40 +0400

    else one refactoring

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm'," HEll0 EVERYONE")
+    print('STRING HERE \n')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
