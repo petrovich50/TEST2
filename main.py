18ed0125f40486e133a27bedd10a83d0c4351eac
DBandurin <dbandurin@gmail.com>
2022-05-06 12:49:49 +0400

    Print added

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     print('STRING HERE \n')
     m = 10
     print(m,' var m \n')
-    print('CATS AND DOGS LIVE iin PEACE')
+    print('CATS AND DOGS LIVE IN PEACE')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-
 print('Hello tetst.py')
+f = 'fgfggfgf'
+f.index('fgg', 0, 20)
+if f in 'fgfgggf':
+    print('YES!')
+else:
+    print('NO')
