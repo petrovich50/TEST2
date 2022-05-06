0051b921a7d5665b5724dbae265c66126ac919f5
DBandurin <dbandurin@gmail.com>
2022-05-06 18:44:41 +0400

    STRING REVERTING

--- test.py
+++ test.py
@@ -7,4 +7,32 @@ else:
     print('NO')
 
 a = 'zyxwvutsrqponmlkjihgfedcba'
-print(a[-1::-1])
+a = a[::-1]
+print(len(a))
+print(a.split())
+list = []
+dict = {}
+for i in range(0, len(a)):
+    list.append(a.__getitem__(i))
+    dict[i] = a.__getitem__(i)
+
+print(list)
+print(dict)
+print('::'.join(list))
+str = 'a famous duck goes into a famous bar...'
+print(str.replace('a', 'a famous', 100))
+import unicodedata
+
+print(unicodedata.name('\u20ac'))
+print(unicodedata.lookup(unicodedata.name('\u20ac')))
+print(' earth k))!)'.strip(')!'))
+import string
+
+prospector = "What in tarnation ...??!!"
+print(prospector.strip(string.whitespace + string.punctuation))
+print(string.whitespace + string.punctuation)
+print('%s' % 7.03)
+cat = 'Chester'
+actor = 'Richard Gere'
+weight = 28
+print("Our cat %s weighs %s pounds" % (cat, weight))
