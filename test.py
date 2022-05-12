cd44060d507f467111d2e4b2b91dd886043b2d81
DBandurin <dbandurin@gmail.com>
2022-05-12 14:58:37 +0400

    STRING REVERTING

--- test.py
+++ test.py
@@ -36,3 +36,15 @@ cat = 'Chester'
 actor = 'Richard Gere'
 weight = 28
 print("Our cat %s weighs %s pounds" % (cat, weight))
+
+
+def print_if_true(thing, check):
+    '''
+    Prints the first argument if a second argument is true.
+    The operation is:
+    1. Check whether the *second* argument is true.
+    2. If it is, print the *first* argument.
+    '''
+    if check:
+        print(thing)
+help()
