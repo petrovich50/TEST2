81eca8dacb01f2b72f9baf344cd3c674f286664a
DBandurin <dbandurin@gmail.com>
2022-06-04 23:05:41 +0400

    New file

--- Test2.py
+++ Test2.py
@@ -2,6 +2,11 @@ import locale
 from datetime import date
 
 halloween = date(2019, 10, 31)
-for lang_country in ['en_us', 'fr_fr', 'de_de', 'es_es', 'is_is']:
+for lang_country in ['en_us', 'fr_fr', 'de_de', 'es_es', 'is_is','ru_ru']:
     locale.setlocale(locale.LC_TIME, lang_country)
-    halloween.strftime('%A, %B %d')
+    print(halloween.strftime('%A, %B %d'))
+
+#print(locale.getlocale(category=locale.LC_ALL))
+
+import os
+os.getgid()
